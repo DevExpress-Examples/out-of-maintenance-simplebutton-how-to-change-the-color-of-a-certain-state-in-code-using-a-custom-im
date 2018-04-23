1dc441d1aede9e1ba500768c3f6578704a88eab3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/dxSample_CustomSimpleButton/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Linq
+Imports DevExpress.LookAndFeel
+Imports DevExpress.Skins
+
+Namespace dxSample_CustomSimpleButton
+    Partial Public Class Form1
+        Inherits DevExpress.XtraEditors.XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+            Dim skin = GetCurrentSkin()
+            SetElementImage(skin, GetImage())
+        End Sub
+        Private Sub SetElementImage(ByVal skin As Skin, ByVal img As Image)
+            If img Is Nothing Then
+                Return
+            End If
+            Dim element = skin(CommonSkins.SkinButton)
+            element.SetActualImage(img, False)
+        End Sub
+        Private Function GetImage() As Image
+            Return My.Resources.button1
+        End Function
+        Private Function GetCurrentSkin() As Skin
+            Return CommonSkins.GetSkin(UserLookAndFeel.Default.ActiveLookAndFeel)
+        End Function
+    End Class
+End Namespace
